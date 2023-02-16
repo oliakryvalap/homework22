9cc49f77c0f5db5afc14389616d9cc71453c8d4c
Olga <olia_tarasenko@inbox.ru>
2023-02-17 02:22:31 +0300

    Задача 3

---
+++ number2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+Console.WriteLine("Введите число");
+string example = Console.ReadLine();
+int a = Convert.ToInt32(example);
+int result = -1;
+if (a >= 100)
+{
+While (a > 99) 
+{a = a/10;
+Console.WriteLine($"число {a % 10}");
+}
+else
+    Console.WriteLine($"число {a}");
+}
+else {
+     int result = a % 10;
---
+++ number3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая принимает на вход цифру,
+ //обозначающую день недели, и проверяет, является ли этот день
+ // выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+ Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 1 && num <=7);
+{
+    if (num == 6 | num == 7)Console.WriteLine("Да");
+    else Console.WriteLine("Нет"); 
+}
