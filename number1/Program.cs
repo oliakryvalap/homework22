b9dcb36c04c50568f4fe3147d7887f8e0caff9df
Olga <olia_tarasenko@inbox.ru>
2023-02-17 01:26:00 +0300

    Задача 1

---
+++ number1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5 
+// 782 -> 8
+// 918 -> 1
+Random createNumber = new Random(); 
+int number = createNumber.Next(100,1000);
+Console.WriteLine($"The first number is {number}");
+int b = number / 10 % 10;
+
+Console.WriteLine($"The number is {b}");
